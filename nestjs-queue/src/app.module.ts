32a63690286e99da2e76d1dfbcee5104abc19ded
ChampKatika <inform.katika@gmail.com>
2022-05-15 20:16:59 +0700

    fix local and docker env

--- nestjs-queue/src/app.module.ts
+++ nestjs-queue/src/app.module.ts
@@ -1,27 +1,53 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { BullModule } from '@nestjs/bull';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
 import { MlWorkerModule } from './ml-worker/ml-worker.module';
 import entities from './typeorm';
 import { Logs } from './typeorm';
 
 @Module({
-  // load config form docker
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'db',
-      port: 3306,
-      // host: 'localhost',
-      // port: 3307,
-      username: 'root',
-      password: 'root',
-      database: 'test_db',
-      entities,
-      synchronize: true,
-      logger: 'advanced-console',
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        // console.log(process.env);
+        return {
+          type:
+            process.env.DB_MYSQL_TYPE ||
+            configService.get<string>('DB_MYSQL_TYPE'),
+          host:
+            process.env.DB_MYSQL_HOST ||
+            configService.get<string>('DB_MYSQL_HOST'),
+          port:
+            parseInt(process.env.DB_MYSQL_PORT) ||
+            configService.get<number>('DB_MYSQL_PORT'),
+          // host: 'localhost',
+          // port: 3307,
+          username:
+            process.env.DB_MYSQL_USER ||
+            configService.get<string>('DB_MYSQL_USER'),
+
+          password:
+            process.env.DB_MYSQL_PASS ||
+            configService.get<string>('DB_MYSQL_PASS'),
+
+          database:
+            process.env.DB_MYSQL_DBNAME ||
+            configService.get<string>('DB_MYSQL_DBNAME'),
+
+          entities,
+          synchronize: true,
+          logger: 'advanced-console',
+        } as TypeOrmModuleOptions;
+      },
     }),
     TypeOrmModule.forFeature([Logs]),
     BullModule.forRoot({
--- nestjs-queue/src/app.service.ts
+++ nestjs-queue/src/app.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-
 import { Logs } from './typeorm/';
+
 @Injectable()
 export class AppService {
   constructor(
