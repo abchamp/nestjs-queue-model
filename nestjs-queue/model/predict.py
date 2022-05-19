ce33c32a08857eb53732e73d5dad220933205da2
ChampKatika <inform.katika@gmail.com>
2022-05-19 14:46:44 +0700

    MLOps nestqueue dogVsCat image start.

--- nestjs-queue/model/predict.py
+++ nestjs-queue/model/predict.py
@@ -8,6 +8,7 @@ import os
 import random
 import shutil
 import pathlib
+import sys
 
 from keras_preprocessing.image import load_img, img_to_array, ImageDataGenerator
 from keras.applications.vgg16 import preprocess_input
@@ -15,44 +16,45 @@ from keras.applications.vgg16 import preprocess_input
 # input image size 150x150
 
 
-def load_and_predict():
-    testImage = load_img(f"./input_test/all_classes/1.jpg", target_size=(150, 150))
+def load_and_predict(modelh5Path, testingDirPath):
+    # testImage = load_img(fullImagePath, target_size=(150, 150))
     # model = keras.models.load_model('dogs-vs-cats.h5')
-    model = keras.models.load_model('dogs-vs-cats.h5')
+    model = keras.models.load_model(modelh5Path)
 
     #preprocess the image
-    testImage = img_to_array(testImage)
-    testImage = testImage.reshape((1, testImage.shape[0], testImage.shape[1], testImage.shape[2]))
+    # testImage = img_to_array(testImage)
+    # testImage = testImage.reshape((1, testImage.shape[0], testImage.shape[1], testImage.shape[2]))
     # testImage = preprocess_input(testImage)
     # testImage = testImage.reshape((1, testImage.shape[0], testImage.shape[1], testImage.shape[2]))
 
-    # test_generator = ImageDataGenerator(rescale=1. / 255)
+    test_generator = ImageDataGenerator(rescale=1. / 255)
 
-    # test_iterator = test_generator.flow_from_directory(
-    #     './input_test',
-    #     target_size=(150, 150),
-    #     shuffle=False,
-    #     class_mode='binary',
-    #     batch_size=1)
+    test_iterator = test_generator.flow_from_directory(
+        testingDirPath,
+        target_size=(150, 150),
+        shuffle=False,
+        class_mode='binary',
+        batch_size=1)
 
-    # ids = []
-    # for filename in test_iterator.filenames:
-    #     ids.append(int(filename.split('\\')[1].split('.')[0]))
+    ids = []
+    for filename in test_iterator.filenames:
+        ids.append(int(filename.split('\\')[1].split('.')[0]))
 
-    predict_result = model.predict(testImage)
+    predict_result = model.predict(test_iterator, steps=len(test_iterator.filenames))
     predictions = []
-    # for index, prediction in enumerate(predict_result):
-    #     predictions.append([ids[index], prediction[0]])
-    # predictions.sort()
+    for index, prediction in enumerate(predict_result):
+        predictions.append([ids[index], prediction[0]])
+    predictions.sort()
 
-    return predict_result
+    return predictions
 
 if __name__ == '__main__':
     try:
-        print("check in")
+        modelh5Path = sys.argv[1]
+        testingDirPath = sys.argv[2]
         # get file path from arg and call
-        # predictions = load_and_predict()
-        # print(predictions)
+        predictions = load_and_predict(modelh5Path, testingDirPath)
+        print(predictions)
     except Exception as e: 
         print(e)    
     
--- nestjs-queue/src/ml-worker/ml-worker.services.ts
+++ nestjs-queue/src/ml-worker/ml-worker.services.ts
@@ -13,6 +13,9 @@ export class MLWorkerService {
     let _pythonModelPath =
       process.env.PYPHON_MODEL_PATH ||
       this.configService.get<string>('PYPHON_MODEL_PATH');
+    let _Modelh5Path =
+      process.env.PYPHON_MODEL_H5_PATH ||
+      this.configService.get<string>('PYPHON_MODEL_H5_PATH');
     let _rawDataDir =
       process.env.RAW_DATA_UPLOADS_DIR ||
       this.configService.get<string>('RAW_DATA_UPLOADS_DIR');
@@ -23,35 +26,38 @@ export class MLWorkerService {
     // php:/uploads/raw_data/abcdefg.jpg to nestjs:/uploads/test_input/all_classes/1.jpg
     //
     try {
-      console.log(__dirname)
-      let _mainTestingFile = `${_testingDataDir}test.${rawFileExt}`;
+      let _mainTestingFile = `${_testingDataDir}/all_classes/1.${rawFileExt}`;
       // await fse.ensureFile(`${_rawDataDir}${rawFileName}`);
+
       await fse.copy(
-        `${_rawDataDir}/${rawFileName}.${rawFileExt}`,
+        `${_rawDataDir}${rawFileName}.${rawFileExt}`,
         _mainTestingFile,
       );
 
+      console.log(_testingDataDir);
+
       const childPros = child.spawn(_pythonScriptPath, [
         _pythonModelPath, // python script
-        _mainTestingFile,
+        _Modelh5Path,
+        _testingDataDir,
       ]);
       let resp_data = '';
       // childPros.stdin.write(JSON.stringify(JSON.stringify(inputData)));
       // childPros.stdin.end();
       for await (const chunk of childPros.stdout) {
-        console.log('stdout chunk: ' + chunk);
+        console.log('=> stdout chunk: ' + chunk);
         resp_data += chunk;
       }
       let error = '';
       for await (const chunk of childPros.stderr) {
-        console.error('stderr chunk: ' + chunk);
+        // console.error('=> stderr chunk: ' + chunk);
         error += chunk;
       }
       const exitCode = await new Promise((resolve, reject) => {
         childPros.on('close', resolve);
       });
       if (exitCode) {
-        throw new Error(`subprocess error exit ${exitCode}, ${error}`);
+        throw new Error(`**** subprocess error exit ${exitCode}, ${error}`);
       }
       //
       // clear on directory
